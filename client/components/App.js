65f199a72fc45ea1626efac05265fadde6a9c31d
Andy Tsai <andy@Andy-Tsai.local>
2016-10-19 02:14:05 +0800

    1. add buttons 2. add weather image

--- client/components/App.js
+++ client/components/App.js
@@ -5,27 +5,55 @@ export default class App extends Component{
     constructor() {
         super()
         this.getWeather = this.getWeather.bind(this)
+        this.handleClick = this.handleClick.bind(this)
         this.state = {
-            url: 'http://localhost:3000/getWeather/1',
             list: []
         }
     }
 
     componentDidMount() {
-        this.getWeather(this.state.url)
+        this.getWeather('/getWeather/1')
     }
 
     render() {
 
         const list = this.state.list.map((obj) => {
-            return <div key={ obj.id }>{ obj.city } { obj.temperature } { obj.rainProbability } { obj.weather }</div>
+            return (
+                <div key={ obj.id }>
+                    { obj.city }
+                    { obj.temperature }
+                    { obj.rainProbability }
+                    { obj.weather }
+                    <img src={obj.weatherImg} alt="" />
+                </div>
+            )
         })
+
+        let buttonNames = ['今日白天', '今晚明晨', '明日白天']
+
+        const hours = new Date().getHours()
+
+        if (hours >= 18 || hours <= 6) {
+            buttonNames = ['今晚明晨', '明日白天', '明日晚上']
+        }
+
+        const buttons = buttonNames.map((name, idx) => {
+            return (
+                <button onClick={ () => this.handleClick( idx+1 ) }>{ name }</button>
+            )
+        })
+
 
         return (
             <div>
+                { buttons }
                 { list }
             </div>
         )
+    }
+
+    handleClick(status) {
+        this.getWeather('/getWeather/' + status)
     }
 
     getWeather(url) {
